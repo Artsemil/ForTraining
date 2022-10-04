7706ff4cebe4e0a7f011c5b6c1a2e26f97323c84
VITALYALGOSEC <96118544+VITALYALGOSEC@users.noreply.github.com>
2022-10-04 12:44:43 +0300

    Update firewall-ingress.tf

--- src/firewall-ingress.tf
+++ src/firewall-ingress.tf
@@ -4,11 +4,12 @@ resource "google_compute_firewall" "i21-ni-gcp" {
 
   allow {
     protocol = "tcp"
-    ports = [
-      "22"]
+    ports = ["22"]
   }
 
   priority = 1500
   source_ranges = [
-    "192.168.0.0/16"]
+    "192.168.0.0/16",
+    "10.168.0.0/16"
+    ]
 }
