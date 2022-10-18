81ffe253712a2d01631fd27a9150de1fe86adf01
artsem.ilyayeu <artsem.ilyayeu@algosec.com>
2022-10-18 09:59:37 +0300

    added deny

--- src/firewall-ingress.tf
+++ src/firewall-ingress.tf
@@ -2,6 +2,11 @@ resource "google_compute_firewall" "i01-ni-gcp" {
   name = "devsecops-fw-all"
   network = "devsecops-network-gcp"
 
+
+  deny {
+    protocol  = "udp"
+  }
+
   description = "INGRESS"
   direction = "INGRESS"
   priority = 1000
