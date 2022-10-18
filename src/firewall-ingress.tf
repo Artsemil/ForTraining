2e9109aaea2f4bc7a6e89e3c92dd1b895611e538
artsem.ilyayeu <artsem.ilyayeu@algosec.com>
2022-10-18 09:57:25 +0300

    removed allow (all by default)

--- src/firewall-ingress.tf
+++ src/firewall-ingress.tf
@@ -2,10 +2,6 @@ resource "google_compute_firewall" "i01-ni-gcp" {
   name = "devsecops-fw-all"
   network = "devsecops-network-gcp"
 
-  allow {
-    protocol = "all"
-  }
-
   description = "INGRESS"
   direction = "INGRESS"
   priority = 1000
